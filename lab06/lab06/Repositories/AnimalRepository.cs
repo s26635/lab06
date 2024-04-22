f211afab07ede61d430bc42c8c184b0d007a06cb
s26635 <s26635@pjwstk.edu.pl>
2024-04-22 11:29:11 +0200

    mmm

--- lab06/lab06/Repositories/AnimalRepository.cs
+++ lab06/lab06/Repositories/AnimalRepository.cs
@@ -29,13 +29,11 @@ public class AnimalRepository : IAnimalRepository
                         string category = reader["Category"].ToString();
                         string area = reader["Area"].ToString();
                         _animals.Add(new Animal(idAnimal, name, description, category, area));
-                        return _animals;
                     }
+                    return _animals;
                 }
             }
         }
-
-        return _animals;
     }
 
     public Animal GetAnimal(int id)
@@ -48,17 +46,17 @@ public class AnimalRepository : IAnimalRepository
             connection.Open();
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                command.Parameters.AddWithValue("@categoryID", 1);
+                command.Parameters.AddWithValue("@IdAnimal", id);
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        int idAnimal = (int)reader["IdAnimal"];
+                        
                         string name = reader["Name"].ToString();
                         string description = reader["Description"].ToString();
                         string category = reader["Category"].ToString();
                         string area = reader["Area"].ToString();
-                        return new Animal(idAnimal, name, description, category, area);
+                        return new Animal(id, name, description, category, area);
                         
                     }
                 }
@@ -70,7 +68,24 @@ public class AnimalRepository : IAnimalRepository
 
     public Animal AddAnimal(AnimalDTO animalDTO)
     {
-        throw new NotImplementedException();
+        string connectionString = "Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=2019SBD;Integrated Security=True;TrustServerCertificate=True";
+
+        string query = "insert into Animal values (@name,@description,@category,@area)";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+
+                command.Parameters.AddWithValue("@name", animalDTO.Name);
+                command.Parameters.AddWithValue("@description", animalDTO.Description);
+                command.Parameters.AddWithValue("@category", animalDTO.Category);
+                command.Parameters.AddWithValue("@area", animalDTO.Area);
+                command.ExecuteReader();
+            }
+        }
+
+        return null;
     }
 
     public void UpdateAnimal(int idAnimal, AnimalDTO animalDTO)
@@ -80,6 +95,18 @@ public class AnimalRepository : IAnimalRepository
 
     public void DeleteAnimal(int id)
     {
-        throw new NotImplementedException();
+        string connectionString = "Data Source=db-mssql16.pjwstk.edu.pl;Initial Catalog=2019SBD;Integrated Security=True;TrustServerCertificate=True";
+
+        string query = "DELETE FROM Animal WHERE IdAnimal=@IdAnimal";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@IdAnimal", id);
+                command.ExecuteReader();
+            }
+        }
+
     }
 }
--- lab06/lab06/Services/AnimalService.cs
+++ lab06/lab06/Services/AnimalService.cs
@@ -22,7 +22,7 @@ internal class AnimalService : IAnimalService
             
         }
         
-        return list;
+        return _animalRepository.GetAnimals();
     }
     
 
